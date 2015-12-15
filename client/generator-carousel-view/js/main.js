bda459950d30bd0d820a5cb014f1dd28ba9bee64
Sean Tracey <seanmtracey@gmail.com>
2015-12-15 16:36:04 +0000

    Add origin of viewing page when carousel is being created
    Allows us to transform URLs for our generators

--- client/generator-carousel-view/js/main.js
+++ client/generator-carousel-view/js/main.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const Carousel = require('ftlabs-screens-carousel');
-const carousel = new Carousel(location.toString());
+const carousel = new Carousel(location.toString(), window.location.origin);
 const iframe = document.createElement('iframe');
 document.body.appendChild(iframe);
 carousel.on('change', url => iframe.src = url);
