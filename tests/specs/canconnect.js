fe8f7c33fd34bdedb4f9dbede0af31975072b1df
Ada Rose Edwards <ada.edwards@ft.com>
2015-12-21 18:33:42 +0000

    Add basic webdriver test

---
+++ tests/specs/canconnect.js
@@ -0,0 +1,23 @@
+'use strict';
+/*global describe, it, browser*/
+
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+describe('Big FT website', () => {
+	it('has "FT Screens" as the heading', function () {
+
+		const title = browser
+			.url('/admin')
+			.waitForText('h1.o-header__title')
+			.getText('h1.o-header__title')
+			.then(title => title)
+			.then(title => new Promise(resolve => setTimeout(resolve(title), 5000)));
+
+		return expect(title).to.eventually.equal('FT Screens');
+	});
+});
