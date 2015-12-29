449621aa899515d3d0590069298e416793cde6de
Ada Rose Edwards <ada.edwards@ft.com>
2015-12-29 11:17:21 +0000

    Code deuplication

--- tests/integration/lib/tabs.js
+++ tests/integration/lib/tabs.js
@@ -9,53 +9,35 @@ module.exports = function(client) {
 	.getCurrentTabId()
 	.then(handle => handles.about = handle)
 	.newWindow('/admin')
-	.getUrl().then(url => console.log('New tab with url: ' + url))
 	.waitForExist('h1.o-header__title')
 	.getCurrentTabId()
 	.then(handle => handles.admin = handle)
 	.newWindow('/viewer')
-	.getUrl().then(url => console.log('New tab with url: ' + url))
 	.waitForExist('#hello .screen-id')
 	.getCurrentTabId()
-	.then(handle => handles.viewer = handle)
-	.then(() => console.log(handles));
+	.then(handle => handles.viewer = handle);
 
-	console.log(loaded);
+	function loadTab(name) {
+		return loaded
+		.getCurrentTabId()
+		.then(id => {
+			if (id !== handles[name]) {
+				return loaded
+				.switchTab(handles[name]);
+			}
+		});
+	}
 	
 	// open a new tab set it to the viewer
 	return {
 		admin() {
-			return loaded
-			.getCurrentTabId()
-			.then(id => {
-				if (id !== handles.admin) {
-					return loaded
-					.switchTab(handles.admin)
-					.getUrl().then(url => console.log('Switching to tab with url: ' + url));
-				}
-			});
+			return loadTab('admin');
 		},
 		viewer() {
-			return loaded
-			.getCurrentTabId()
-			.then(id => {
-				if (id !== handles.viewer) {
-					return loaded
-					.switchTab(handles.viewer)
-					.getUrl().then(url => console.log('Switching to tab with url: ' + url));
-				}
-			});
+			return loadTab('viewer');
 		},
 		about() {
-			return loaded
-			.getCurrentTabId()
-			.then(id => {
-				if (id !== handles.about) {
-					return loaded
-					.switchTab(handles.about)
-					.getUrl().then(url => console.log('Switching to tab with url: ' + url));
-				}
-			});
+			return loadTab('about');
 		}
 	}
 };
