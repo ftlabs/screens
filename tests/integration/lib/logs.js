f6f55510e2d54d8e867ddea608c1a61c85013413
Ada Rose Edwards <ada.edwards@ft.com>
2015-12-31 17:40:04 +0000

    trim

--- tests/integration/lib/logs.js
+++ tests/integration/lib/logs.js
@@ -4,7 +4,11 @@ module.exports = function(client) {
 	return function () {
 		return client.log('browser')
 		.then(function (logs) {
-			return logs.value.filter(i => !!i).map(v => `${(new Date(Number(v.timestamp))).toTimeString()}: ${v.message}`).join('\n');
+			return logs.
+				value
+				.filter(i => !!i.trim())
+				.map(v => `${(new Date(Number(v.timestamp))).toTimeString()}: ${v.message}`)
+				.join('\n');
 		});
 	};
 };
