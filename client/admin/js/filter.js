0aab56406fb35b0eeae2cb2baeb1f93df54505c7
Ada Rose Edwards <ada@ada.is>
2016-06-24 14:11:43 +0100

    Use leading debounce to avoide flashwhen screen changes

--- client/admin/js/filter.js
+++ client/admin/js/filter.js
@@ -1,36 +1,38 @@
 let $;
-let timer;
 let $txt;
 
+const debounce = require('lodash.debounce');
+
 function applyFilters() {
-	clearTimeout(timer);
-	timer = setTimeout(function() {
-		if ($txt.val()) {
-			const regex = new RegExp('^(.*?)('+$txt.val()+')(.*?)$', 'ig');
-			const $matching = $('.screen[data-filter*="'+$txt.val().toLowerCase()+'"]');
-			$matching.show().find('.screen-name').each(function() {
-				$(this).html(this.title.replace(regex, '$1<span class="highlight">$2</span>$3'));
-			});
-			$('.screen').not($matching).hide();
-			if ($matching.length === 1 && !$matching.find('.screen-select').prop('checked')) {
-				$matching.find('.screen-select').prop('checked', true);
-			}
-		} else {
-			$('.screen').show().find('.screen-name').each(function() {
-				$(this).html(this.title);
-			});
+	if ($txt.val()) {
+		const regex = new RegExp('^(.*?)('+$txt.val()+')(.*?)$', 'ig');
+		const $matching = $('.screen[data-filter*="'+$txt.val().toLowerCase()+'"]');
+		$matching.show().find('.screen-name').each(function() {
+			$(this).html(this.title.replace(regex, '$1<span class="highlight">$2</span>$3'));
+		});
+		$('.screen').not($matching).hide();
+		if ($matching.length === 1 && !$matching.find('.screen-select').prop('checked')) {
+			$matching.find('.screen-select').prop('checked', true);
 		}
-	}, 200);
+	} else {
+		$('.screen').show().find('.screen-name').each(function() {
+			$(this).html(this.title);
+		});
+	}
 }
+
+const applyFiltersDebounced = debounce(applyFilters, 200, {
+	leading: true
+});
 
 exports.init = function(jQuery) {
 	$ = jQuery;
 	$txt = $('#txtfilter');
 	$txt.on('keyup', function(e) {
 		e.preventDefault();
-		applyFilters();
+		applyFiltersDebounced();
 	});
-	applyFilters();
+	applyFiltersDebounced();
 };
 
-exports.apply = applyFilters;
+exports.apply = applyFiltersDebounced;
--- client/viewer/js/main.js
+++ client/viewer/js/main.js
@@ -25,7 +25,6 @@ const storage = {
 		} else {
 			callback( JSON.parse( info ) );
 		}
-
 	}
 };
 
