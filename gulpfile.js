9814502227c962fd21c6b786f34efca9b6ba5b95
Alex Wilson <a@ax.gy>
2019-02-22 00:17:29 +0000

    chore: Update to Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,100 @@
 /* global console */
 'use strict'; //eslint-disable-line strict
+const fs = require('fs');
 const gulp = require('gulp');
 const obt = require('origami-build-tools');
 const spawn = require('child_process').spawn;
 let node;
 
-gulp.task('serve', function() {
+gulp.task('serve', gulp.series(done => {
 	if (node) node.kill();
 	node = spawn('bin/www', [], {stdio: 'inherit'});
 	console.log('Spawned server as PID '+node.pid);
 	node.on('exit', function (code, signal) {
 		console.log('Service exit. '+code+' '+signal);
+		done()
 	});
 	node.on('close', function (code, signal) {
 		console.log('Service close I/O. '+code+' '+signal);
 	});
-});
+}));
 
 function build(app) {
-	return obt.build(gulp, {
-		js: './client/'+app+'/js/main.js',
-		sass: './client/'+app+'/scss/main.scss',
-		buildJs: 'bundle.js',
-		buildCss: 'bundle.css',
-		buildFolder: 'public/build/'+app
-	});
+	const obtConfig = {
+		buildFolder: `public/build/${app}`
+	};
+
+	const jsPath = `./client/${app}/js/main.js`
+	if (fs.existsSync(jsPath)) {
+		obtConfig.js = jsPath;
+		obtConfig.buildJs = 'bundle.js';
+	}
+
+	const sassPath = `./client/${app}/scss/main.scss`
+	if (fs.existsSync(sassPath)) {
+		obtConfig.sass = sassPath
+		obtConfig.buildCss = 'bundle.css';
+	}
+
+	return obt.build(gulp, obtConfig);
 }
 
-gulp.task('buildLogs', function () {
-	return build('logs');
+gulp.task('buildLogs', done => {
+	build('logs');
+	done();
 });
 
-gulp.task('buildAdmin', function() {
-	return build('admin');
+gulp.task('buildAdmin', done => {
+	build('admin');
+	done();
 });
 
-gulp.task('buildViewer', function() {
-	return build('viewer');
+gulp.task('buildViewer', done => {
+	build('viewer');
+	done();
 });
 
-gulp.task('buildGeneratorLayoutView', function () {
-	return build('generator-layout-view');
+gulp.task('buildGeneratorLayoutView', done => {
+	build('generator-layout-view');
+	done();
 });
 
-gulp.task('buildGeneratorLayoutAdmin', function () {
-	return build('generator-layout-admin');
+gulp.task('buildGeneratorLayoutAdmin', done => {
+	build('generator-layout-admin');
+	done();
 });
 
-gulp.task('buildGeneratorCarouselView', function () {
-	return build('generator-carousel-view');
+gulp.task('buildGeneratorCarouselView', done => {
+	build('generator-carousel-view');
+	done();
 });
 
-gulp.task('buildGeneratorCarouselAdmin', function () {
-	return build('generator-carousel-admin');
+gulp.task('buildGeneratorCarouselAdmin', done => {
+	build('generator-carousel-admin');
+	done();
 });
 
-gulp.task('buildGeneratorRtcView', function () {
-	return build('generator-rtc-view');
+gulp.task('buildGeneratorRtcView', done => {
+	build('generator-rtc-view');
+	done();
 });
 
-gulp.task('buildGeneratorRtcAdmin', function () {
-	return build('generator-rtc-admin');
+gulp.task('buildGeneratorRtcAdmin', done => {
+	build('generator-rtc-admin');
+	done();
 });
 
-gulp.task('buildGeneratorYoutube', function () {
-	return build('generator-youtube-player');
+gulp.task('buildGeneratorYoutube', done => {
+	build('generator-youtube-player');
+	done();
 });
 
-gulp.task('buildGenerators', ['buildGeneratorLayoutView', 'buildGeneratorLayoutAdmin', 'buildGeneratorCarouselView', 'buildGeneratorCarouselAdmin', 'buildGeneratorRtcView', 'buildGeneratorRtcAdmin', 'buildGeneratorYoutube']);
+gulp.task('buildGenerators', gulp.parallel('buildGeneratorLayoutView', 'buildGeneratorLayoutAdmin', 'buildGeneratorCarouselView', 'buildGeneratorCarouselAdmin', 'buildGeneratorRtcView', 'buildGeneratorRtcAdmin', 'buildGeneratorYoutube'));
 
-gulp.task('build', ['buildLogs', 'buildAdmin', 'buildViewer', 'buildGenerators']);
+gulp.task('build', gulp.parallel('buildLogs', 'buildAdmin', 'buildViewer', 'buildGenerators'));
 
-gulp.task('verify', function() {
-	return obt.verify(gulp, {
+gulp.task('verify', done => {
+	obt.verify(gulp, {
 
 		// Files to exclude from Origami verify
 		excludeFiles: [
@@ -80,14 +102,17 @@ gulp.task('verify', function() {
 			'!client/admin/scss/lib/**' //
 		]
 	});
+	done();
 });
 
-gulp.task('watch', ['build', 'serve'], function() {
+gulp.task('watch', gulp.series('build', 'serve', done => {
 	gulp.watch('./client/**/*', ['build']);
 	gulp.watch('./server/**/*', ['serve']);
 	gulp.watch('./views/**/*', ['serve']);
-});
+	done();
+}));
 
-gulp.task('default', ['verify'], function() {
+gulp.task('default', gulp.series('verify', done => {
 	gulp.run('watch');
-});
+	done();
+}));
